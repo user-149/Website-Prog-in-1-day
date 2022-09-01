6259c4598190983a1901f03a217bfc806081a494
Code-Monkey_149 <55069803+user-149@users.noreply.github.com>
2022-09-01 16:48:53 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -20,6 +20,29 @@
 			Navbarnya akan "Stick" ketika sedang di scroll
 		</p> -->
 		<p>
+			'Welcome to the internet!'
+		</p>
+		<p>
+			berikut ini adalah hasil dari Training Kilat User149 A.K.A Code-Monkey_149 dari PT.YE*D bersama Mr. Subhan from WEBHOZZ yang mana saya ditugaskan untuk membuat WEBSITE dalam 1 hari. hehehe.
+		</p>
+		<p>
+			jika anda melihat ini maka anda di perbolehkan untuk menambah data atau memodifikasi data. harap di ingat bahwa beberapa data saling tehubung 1 sama lain dan belum bisa dilakukan auto-update jika data yang sudah di setting tersebut hilang atau bertambah, oleh karena itu silahkan dijadikan referensi untuk dipelajari.
+		</p>
+		<p>
+			akhir kata, selamat berCODING ria,
+			<br>
+			<br>siapkan posisi paling nyaman,
+			<br>standby kan kopi & snack dalam jangkauan tangan,
+			<br>Pasang headset dan Play musik produktif mu (INI PENTING!),
+			<br>relax-kan seluruh tubuh dan yang paling utama jemari tangan sebelum code marathon,
+			<br>silahkan berdoa supaya bisa menemukan solusi di stack overflow untuk ssemua error yang ditemui,
+			<br>sediakan air mineral dalam jangkauan tangan incase terjadi typo atau lupa menambahkan ';' di akhir kode akibat dari dehidrasi (stay hydrate guys!)
+			<br>
+			<br>dan...
+			<br>
+			<br>Cek ki CODE...
+		</p>
+		<p>
 			Some text to enable scrolling. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.
 		</p>
 		<p>
